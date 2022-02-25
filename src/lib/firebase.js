ac1ae329f3e696ea661081aeb59137ccdf11afcc
DanielaAlcalaDaboin <daniela.alcalad@gmail.com>
2022-02-25 11:49:07 -0300

    Se activo el archivo de config.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,17 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-analytics.js";
+import { getDatabase } from "https://DATABASE_NAME.southamerica-east1.firebaseio.com";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-analytics.js";
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-auth.js";
 import {firebaseConfig} from "./config.js"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+
+// const analytics = getAnalytics(app);
 
 
 const auth = getAuth();
@@ -30,3 +29,31 @@ export const registerUser = (email, password) =>{
     // ..
   });
 }
+
+const database = getDatabase(app);
+export const dataUser = (nameLastname, dateOfbirth) =>{
+  // Constante original      const database = getDatabase(app);
+   getDatabase (database, nameLastname, dateOfbirth);
+}
+
+
+
+// import { getDatabase } from "firebase/database";
+
+// // TODO: Replace with your app's Firebase project configuration
+// const firebaseConfig = {
+//   apiKey: "API_KEY",
+//   authDomain: "PROJECT_ID.firebaseapp.com",
+//   // The value of `databaseURL` depends on the location of the database
+//   databaseURL: "https://DATABASE_NAME.firebaseio.com",
+//   projectId: "PROJECT_ID",
+//   storageBucket: "PROJECT_ID.appspot.com",
+//   messagingSenderId: "SENDER_ID",
+//   appId: "APP_ID",
+//   // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+//   measurementId: "G-MEASUREMENT_ID",
+// };
+
+// const app = initializeApp(firebaseConfig);
+
+// Get a reference to the database service
--- src/register.js
+++ src/register.js
@@ -1,17 +1,17 @@
 import { footerCredits } from './lib/index.js';
-import {registerUser} from './lib/firebase.js'
+import { registerUser, dataUser } from './lib/firebase.js';
 
 const container = document.getElementById('root');
 const footer = footerCredits();
 
 export function register() {
-    
+
     console.log('ola k ase');
     history.pushState(null, 'Registro', '/register')
 
 
     const containerRegister = document.createElement('main');
-    containerRegister.setAttribute('id','mainRegister');
+    containerRegister.setAttribute('id', 'mainRegister');
     containerRegister.classList.add('mainRegister');
 
     const subTitle = document.createElement('h2');
@@ -19,16 +19,17 @@ export function register() {
     const formRegister = document.createElement('form');
 
     const inputNameAndLastName = document.createElement('input');
+    inputNameAndLastName.setAttribute('id', 'nameLastname');
     inputNameAndLastName.setAttribute('type', 'text');
     inputNameAndLastName.setAttribute('placeholder', 'Nombre y Apellido');
 
     const inputMail = document.createElement('input');
-    inputMail.setAttribute('id','email');
+    inputMail.setAttribute('id', 'email');
     inputMail.setAttribute('type', 'email');
     inputMail.setAttribute('placeholder', 'Correo electrónico');
 
     const inputPassword = document.createElement('input');
-    inputPassword.setAttribute('id','password');
+    inputPassword.setAttribute('id', 'password');
     inputPassword.setAttribute('type', 'password');
     inputPassword.setAttribute('placeholder', 'Crear contraseña');
 
@@ -51,10 +52,8 @@ export function register() {
     containerRegister.appendChild(subTitle);
     containerRegister.appendChild(formRegister);
     formRegister.appendChild(inputNameAndLastName);
-    formRegister.appendChild(inputUser);
-    formRegister.appendChild(inputPassword);
-    formRegister.appendChild(inputPassword2);
     formRegister.appendChild(inputMail);
+    formRegister.appendChild(inputPassword);
     formRegister.appendChild(labelDate);
     formRegister.appendChild(inputDate);
     formRegister.appendChild(btnSend);
@@ -63,12 +62,93 @@ export function register() {
     formRegister.addEventListener('submit', e => {
         e.preventDefault();
         console.log('ola k ace 2');
-
+        const nameLastname = document.getElementById('nameLastname').value;
+        const dateOfBirth = document.getElementById('date').value;
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
+        
+        console.log(nameLastname);
+        console.log(dateOfBirth);
         console.log(email);
         console.log(password);
-        registerUser(email,password);
+        
+        dataUser(nameLastname,dateOfBirth);
+        registerUser(email, password);
+        
     });
 
-}
+
+
+
+
+
+
+
+
+    // const form = document.getElementById('form');
+
+    // const inputs = document.querySelectorAll('#form input');
+
+    // const expressions = {
+
+    //     nameLastname: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+    //     email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/,
+    //     password: /^.{6,12}$/ // 6 a 12 digitos.
+
+    // };
+
+    // const fields = {
+    //     nameLastname: false,
+    //     email: false,
+    //     password: false
+
+    // };
+
+    // const validationForm = e => {
+    //     // con esta linea tomaremos cada nombre de los inputs para que este sea reconocido
+
+
+    //     switch (e.target.name) {
+    //         /* declaraciones ejecutadas cuando el resultadop de la expresion coincide con el valor asignado en este caso las expresiones (valid) */
+    //         case "nameLastname":
+    //             inputValidation(expressions.nameLastname, e.target, 'nameLastname');
+
+    //             break;
+
+    //         case "email":
+    //             inputValidation(expressions.email, e.target, 'email');
+
+    //             break;
+
+    //         case "password":
+    //             inputValidation(expressions.password, e.target, 'password');
+    //             break;
+    //     };
+    // };
+
+    // const inputValidation = (expressions, input, fields) => {
+    //     if (expressions.test(input.value)) {
+    //         document.getElementById(`grupo__${fields}`).classList.remove('formulario__grupo-incorrecto');
+    //         document.getElementById(`grupo__${fields}`).classList.add('formulario__grupo-correcto');
+    //         document.querySelector(`#grupo__${fields} i`).classList.add('fa-check-circle');
+    //         document.querySelector(`#grupo__${fields} i`).classList.remove('fa-times-circle');
+    //         document.querySelector(`#grupo__${fields} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+
+    //     } else {
+    //         document.getElementById(`grupo__${fields}`).classList.add('formulario__grupo-incorrecto');
+    //         document.getElementById(`grupo__${fields}`).classList.remove('formulario__grupo-correcto');
+    //         document.querySelector(`#grupo__${fields} i`).classList.add('fa-times-circle');
+    //         document.querySelector(`#grupo__${fields} i`).classList.remove('fa-check-circle');
+    //         document.querySelector(`#grupo__${fields} .formulario__input-error`).classList.add('formulario__input-error-activo');
+
+    //     }
+    // }
+
+    // // parametro input para identificar
+    // inputs.forEach((input) => {
+    //     // se hara la funcion cada vez que haya un evento de tecleo dentro de los input
+    //     input.addEventListener('keyup', validationForm);
+    //     input.addEventListener('blur', validationForm);
+    // });
+};
+
