f2bba8ec23d1f6d888fdf8bee674ba92b94f3dbe
DanielaAlcalaDaboin <daniela.alcalad@gmail.com>
2022-03-01 17:22:23 -0300

    Se logro el correo de confirmación

--- src/register.js
+++ src/register.js
@@ -86,6 +86,7 @@ export function register() {
 
         // dataUser(nameLastname,dateOfBirth);
         registerUser(email, password, nameLastname, dateOfBirth);
+        
     })
 
     linkBack.addEventListener('click', () =>{
